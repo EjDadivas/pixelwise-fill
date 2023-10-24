1b5d9e4d336572f6aa48f31899e5b6c7a190f62d
ej <99721216+EjDadivas@users.noreply.github.com>
2023-10-24 14:18:56 +0800

    ignoreDuring Builds

--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ const nextConfig = {
     serverComponentsExternalPackages: ["cloudinary"],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
